cf12f44400fccb3bcef099fae64fe372388619cc
Renszo Camacho <varsovia84@gmail.com>
2021-02-10 14:50:18 +0100

    Check if an Object has a Property.

---
+++ src/js/objectHasProperty.js
@@ -0,0 +1,33 @@
+// Check if an Object has a Property
+/*
+We've created an object, users, with some users in it and a function isEveryoneHere, 
+which we pass the users object to as an argument. Finish writing this function so that it 
+returns true only if the users object contains all four names, 
+Alan, Jeff, Sarah, and Ryan, as keys, and false otherwise.
+*/
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+const isEveryoneHere = (obj) =>
+  "Alan" in obj && "Jeff" in obj && "Sarah" in obj && "Ryan" in obj
+    ? true
+    : false;
+
+console.log(isEveryoneHere(users));
