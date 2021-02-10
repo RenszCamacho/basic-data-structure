4ed1fb3965998242b679aeeabc22bc25e8cf864d
Renszo Camacho <varsovia84@gmail.com>
2021-02-10 14:17:09 +0100

    Access Property Names with Bracket Notation.

--- src/js/Objects.js
+++ src/js/Objects.js
@@ -13,8 +13,38 @@ let foods = {
 foods.bananas = 13;
 foods.grapes = 35;
 foods.strawberries = 27;
-// Only change code below this line
 
-// Only change code above this line
+/*Here we've defined an object userActivity, which includes another object nested within it. 
+  Set the value of the online key to 45.*/
 
-console.log(foods);
+let userActivity = {
+  id: 23894201352,
+  date: "January 1, 2017",
+  data: {
+    totalUsers: 51,
+    online: 42,
+  },
+};
+
+userActivity.data.online = 45;
+
+/*
+We've defined a function, checkInventory, which receives a scanned item as an argument. 
+Return the current value of the scannedItem key in the foods object. 
+You can assume that only valid keys will be provided as an argument to checkInventory.
+*/
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+function checkInventory(scannedItem) {
+  food[scannedItem];
+}
+
+console.log(checkInventory("apples"));
