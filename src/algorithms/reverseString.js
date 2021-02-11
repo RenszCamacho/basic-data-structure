56af122f3d43c6330c0129cffbef8f4c5ef90b75
Renszo Camacho <varsovia84@gmail.com>
2021-02-11 17:37:34 +0100

    Reverse a String.

---
+++ src/algorithms/reverseString.js
@@ -0,0 +1,7 @@
+// Reverse a String
+/*Reverse the provided string.*/
+
+const reverseString = (str) => str.split("").reverse().join("");
+
+const result = reverseString("Greetings from Earth");
+console.log(result);
