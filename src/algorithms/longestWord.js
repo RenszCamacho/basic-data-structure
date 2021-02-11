b6c3619bf512862fb2e6bcc07044c97076c545ba
Renszo Camacho <varsovia84@gmail.com>
2021-02-11 18:19:56 +0100

    Find the longest word in a string.

---
+++ src/algorithms/longestWord.js
@@ -0,0 +1,18 @@
+// Find the longest word in a string.
+
+/*
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+*/
+const findLongestWordLength = (str) => {
+  return Math.max(...str.split(" ").map((word) => [...word].length));
+  // const a = str.split(" ");
+  // const b = a.map((e) => e.split("").length);
+  // return Math.max(...b);
+};
+
+const result = findLongestWordLength(
+  "The quick brown fox jumped over the lazy dog"
+);
+console.log(result);
