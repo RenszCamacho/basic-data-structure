997901d87537f385c0cdef57cea39b795e6ed153
Renszo Camacho <varsovia84@gmail.com>
2021-02-14 12:56:55 +0100

    Repeat a String Repeat a String.Recursion

--- src/algorithms/repeatString.js
+++ src/algorithms/repeatString.js
@@ -1,18 +1,22 @@
-// Repeat a String Repeat a String
 /*
 Repeat a given string str (first argument) for num times (second argument). Return an empty string 
 if num is not a positive number. For the purpose of this challenge, do not use the 
 built-in .repeat() method.
 */
 
+//FOR LOOP
 const repeatStringNumTimes = (str, num) => {
   let newStr = str;
   for (let i = 1; i < num; i++) {
     newStr += str;
   }
   return num <= 0 ? "" : newStr;
-  // return num < 0 ? "" : str.repeat(num);
+  return num < 0 ? "" : str.repeat(num);
 };
 
-const result = repeatStringNumTimes("*", 8);
-console.log(result);
+//RECURSION
+const repeatStringNumTimes = (str, num) =>
+  num <= 0 ? "" : str + repeatStringNumTimes(str, num - 1);
+
+// const result = repeatStringNumTimes("*", 8);
+// console.log(result);
