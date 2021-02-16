025df8a7cab64d9bc073337550ff85a8459c5a47
Renszo Camacho <varsovia84@gmail.com>
2021-02-16 12:00:57 +0100

    Title Case a Sentence.

--- src/algorithms/titleCase.js
+++ src/algorithms/titleCase.js
@@ -5,22 +5,13 @@ the word is in lower case.
 
 For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 */
-for (let i = 1; i <= 100; i++) {
-  // if (i % 3 === 0 && i % 5 === 0) {
-  //   console.log("Fizz Buzz");
-  // } else if (i % 3 === 0) {
-  //   console.log("Fizz");
-  // } else if (i % 5 === 0) {
-  //   console.log("Buzz");
-  // } else {
-  //   console.log(i);
-  // }
 
-  i % 3 === 0 && i % 5 === 0
-    ? console.log("Fizz Buzz")
-    : i % 3 === 0
-    ? console.log("Fizz")
-    : i % 5 === 0
-    ? console.log("Buzz")
-    : console.log(i);
-}
+const titleCase = (str) =>
+  str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+
+const result = titleCase("sHoRt AnD sToUt");
+console.log(result);
